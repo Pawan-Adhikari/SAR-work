4d030c2b1697bb87bf766dbac90741e67737a16f
Pawan-Adhikari <pawan.adk7@gmail.com>
2025-09-22 11:54:39 +0545

    Some quality of life improvements

--- Src/DownloadProcessing.py
+++ Src/DownloadProcessing.py
@@ -1,4 +1,4 @@
-# initial setup
+# Imports and libraries
 import asf_search as asf
 import hyp3_sdk
 import re
@@ -7,57 +7,57 @@ from zipfile import ZipFile
 import Crop_Product as cp
 import subprocess
 
-
-
+#Initiation:
+start_date='07-01'
+end_date='07-30'
 hyp3 = hyp3_sdk.HyP3(username='pon.adk', password='qeDriz-juhdu3-feckav')
-
 wkt = 'POLYGON ((86.7425991901464 27.935407268344107, 86.7425991901464 27.93062783962911, 86.75104832773593 27.93062783962911, 86.75104832773593 27.935407268344107, 86.7425991901464 27.935407268344107))'
-
 n = 0
+loc='../SAR_products_unprocessed/newBatch'
+
+#Searching:
 results = []
 results_2025 = asf.geo_search(intersectsWith=wkt,
                          platform=[asf.PLATFORM.SENTINEL1],
                          processingLevel=[asf.PRODUCT_TYPE.GRD_HD,asf.PRODUCT_TYPE.GRD_HS, asf.PRODUCT_TYPE.GRD_MD, asf.PRODUCT_TYPE.GRD_MS, asf.PRODUCT_TYPE.GRD_FD],
-                         start='2025-02-01',
-                         end='2025-03-01')
+                         start='2025-' + start_date,
+                         end='2025-'+end_date)
 results.append(results_2025[n])
-
-
 
 results_2024 = asf.geo_search(intersectsWith=wkt,
                          platform=[asf.PLATFORM.SENTINEL1],
                          processingLevel=[asf.PRODUCT_TYPE.GRD_HD,asf.PRODUCT_TYPE.GRD_HS, asf.PRODUCT_TYPE.GRD_MD, asf.PRODUCT_TYPE.GRD_MS, asf.PRODUCT_TYPE.GRD_FD],
-                         start='2024-02-01',
-                         end='2024-03-01')
+                         start='2024-' + start_date,
+                         end='2024-'+end_date)
 results.append(results_2024[n])
 
 results_2023 = asf.geo_search(intersectsWith=wkt,
                          platform=[asf.PLATFORM.SENTINEL1],
                          processingLevel=[asf.PRODUCT_TYPE.GRD_HD,asf.PRODUCT_TYPE.GRD_HS, asf.PRODUCT_TYPE.GRD_MD, asf.PRODUCT_TYPE.GRD_MS, asf.PRODUCT_TYPE.GRD_FD],
-                         start='2023-02-01',
-                         end='2023-03-01')
+                         start='2023-' + start_date,
+                         end='2023-'+end_date)
 results.append(results_2023[n])
     
 results_2022 = asf.geo_search(intersectsWith=wkt,
                          platform=[asf.PLATFORM.SENTINEL1],
                          processingLevel=[asf.PRODUCT_TYPE.GRD_HD,asf.PRODUCT_TYPE.GRD_HS, asf.PRODUCT_TYPE.GRD_MD, asf.PRODUCT_TYPE.GRD_MS, asf.PRODUCT_TYPE.GRD_FD],
-                         start='2022-02-01',
-                         end='2022-03-01')
+                         start='2022-' + start_date,
+                         end='2022-'+end_date)
 results.append(results_2022[n])
 
 results_2021 = asf.geo_search(intersectsWith=wkt,
                          platform=[asf.PLATFORM.SENTINEL1],
                          processingLevel=[asf.PRODUCT_TYPE.GRD_HD,asf.PRODUCT_TYPE.GRD_HS, asf.PRODUCT_TYPE.GRD_MD, asf.PRODUCT_TYPE.GRD_MS, asf.PRODUCT_TYPE.GRD_FD],
-                         start='2021-02-01',
-                         end='2021-03-01')
+                         start='2021-' + start_date,
+                         end='2021-'+end_date)
 results.append(results_2021[n])
-
 granule_ids = [result.properties['sceneName'] for result in results]
 
+#Display found products
 for result in results:
     print(result)
 
-# Create a list of job dictionaries using prepare_rtc_job
+# Prepare RTC jobs from products
 job_definitions = []
 for granule_id in granule_ids:
     job_definitions.append(
@@ -70,7 +70,7 @@ for granule_id in granule_ids:
     )
 print(job_definitions)
 
-loc='../SAR_products_unprocessed/newBatch'
+#Check and continue with processing
 check = input("Do you want to continue ? (Y/N)")
 if check == 'Y':
     #job = hyp3.submit_rtc_job(granule=granule_ids[0], name='MyNewJob') 
@@ -80,6 +80,7 @@ if check == 'Y':
     jobs_urls = [job.files[0]['url'] for job in jobs]
     print(jobs_urls)
 
+#Check and continue with downloading
     check = input("Do you want to continue Download? (Y/N)")
     if (check == 'Y'): 0
     else: quit()
@@ -88,11 +89,13 @@ if check == 'Y':
         subprocess.run(["wget", "-c", url, "-P", loc])
     #job.download_files(location = loc, create=True)
 
+#Preparing to unzip
 zipPaths = []
 for path in Path(loc).glob("*.zip"):
     zipPaths.append(path)
 
 
+#Start matching and extracting and finally cropping. Matching uncesseary for year wise sampling.
 start_year = 2021
 for i in range (5):
     year = start_year + i
